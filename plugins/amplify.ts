ae079ac4163ac766e0453b6e54262b64fa2b1682
Alexey Tuzov <sugarfox.dev@gmail.com>
2024-03-29 16:51:48 +0300

    rewrite plugin import path to relative

--- plugins/amplify.ts
+++ plugins/amplify.ts
@@ -1,6 +1,6 @@
 import { Amplify } from "aws-amplify"
 import { signIn, signOut, signUp } from "aws-amplify/auth"
-import awsExports from "~/aws-exports"
+import awsExports from "../aws-exports"
 
 export default defineNuxtPlugin(nuxtApp => {
   Amplify.configure(awsExports)
