d152cf683d0177d790de28445e56d369e0e4dde1
Alexey Tuzov <sugarfox.dev@gmail.com>
2024-03-29 15:55:34 +0300

    setup amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "nuxtawstest": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
---
+++ plugins/amplify.ts
@@ -0,0 +1,17 @@
+import { Amplify } from "aws-amplify"
+import { signIn, signOut, signUp } from "aws-amplify/auth"
+import awsExports from "~/aws-exports"
+
+export default defineNuxtPlugin(nuxtApp => {
+  Amplify.configure(awsExports)
+
+  return {
+    provide: {
+      auth: {
+        signIn,
+        signOut,
+        signUp
+      }
+    }
+  }
+})
