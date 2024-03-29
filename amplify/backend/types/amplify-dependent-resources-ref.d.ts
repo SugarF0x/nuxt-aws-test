01aa13debeefc1326baaed0dda0fb89b2d8746ce
Alexey Tuzov <sugarfox.dev@gmail.com>
2024-03-29 16:45:39 +0300

    add dynamo table

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "nuxtawstestamplifyapi": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
   "auth": {
     "nuxtawstest": {
       "AppClientID": "string",
@@ -8,6 +15,27 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "nuxtawsteste3d97cbf": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "dynamo98ca0df4": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "SortKeyName": "string",
+      "SortKeyType": "string",
+      "StreamArn": "string"
     }
   }
 }
---
+++ pages/items.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const { data } = useFetch('/api/dynamo/items')
+</script>
+
+<template>
+  <pre>{{ JSON.stringify(data, null, 2) }}</pre>
+</template>
+
+<style scoped>
+
+</style>
