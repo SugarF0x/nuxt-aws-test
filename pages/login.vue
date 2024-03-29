dd577f8b50302d5939dde98b9f69003208e84109
Alexey Tuzov <sugarfox.dev@gmail.com>
2024-03-29 16:22:58 +0300

    add login

---
+++ pages/login.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { Authenticator } from "@aws-amplify/ui-vue"
+</script>
+
+<template>
+  <authenticator>
+    <template v-slot="{ user, signOut }">
+      <h1>Hello {{ user.username }}</h1>
+      <button @click="signOut()" >cum balls</button>
+    </template>
+  </authenticator>
+</template>
+
+<style src="@aws-amplify/ui-vue/styles.css" />
