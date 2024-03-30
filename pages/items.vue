a77a91c7564e1a3647ce784333cba987ebf456c4
Alexey Tuzov <sugarfox.dev@gmail.com>
2024-03-30 15:36:16 +0300

    add new controls

--- pages/items.vue
+++ pages/items.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-const { data } = await useFetch('/dynamo/items')
+const { data, execute, pending } = await useFetch('/dynamo/items')
 </script>
 
 <template>
+  <pre>pending: {{ pending.toString() }}</pre>
   <pre>{{ JSON.stringify(data, null, 2) }}</pre>
+  <button @click="execute()">retry</button>
 </template>
 
 <style scoped>
