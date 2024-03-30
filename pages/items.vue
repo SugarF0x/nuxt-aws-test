b32f28c926270bfabd26a67e2288d33cc9788f0b
Alexey Tuzov <sugarfox.dev@gmail.com>
2024-03-30 15:42:30 +0300

    titti cakka

--- pages/items.vue
+++ pages/items.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
+import { useAsyncData } from "#app"
+
 const path = ref('/api/items')
 
-const { data, execute, pending } = await useFetch(path.value)
+const { data, execute, pending } = await useAsyncData(() => $fetch(path.value))
 </script>
 
 <template>
-  <input type="text" v-model="path">
+  <input type="text" v-model="path" @keydown.enter="execute()">
   <pre>pending: {{ pending.toString() }}</pre>
   <pre>{{ JSON.stringify(data, null, 2) }}</pre>
   <button @click="execute()">retry</button>
