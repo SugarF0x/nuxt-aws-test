96072da08523c5d97725e5a5f98409282a4a34ec
Alexey Tuzov <sugarfox.dev@gmail.com>
2024-03-30 15:39:54 +0300

    add peepee poopoo

--- pages/items.vue
+++ pages/items.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
-const { data, execute, pending } = await useFetch('/dynamo/items')
+const path = ref('/api/items')
+
+const { data, execute, pending } = await useFetch(path.value)
 </script>
 
 <template>
+  <input type="text" v-model="path">
   <pre>pending: {{ pending.toString() }}</pre>
   <pre>{{ JSON.stringify(data, null, 2) }}</pre>
   <button @click="execute()">retry</button>
